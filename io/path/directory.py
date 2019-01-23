9e7a0cbabb3c850d55694cebc350818831b79d00
Chris Carlson <Chris Carlson>
2019-01-23 10:33:25 -0500

    Split Path into File and Directory

--- io/path/directory.py
+++ io/path/directory.py
@@ -1,13 +1,13 @@
-from pathlib import Path as PythonPath
+from pathlib import Path
 
 
-class Path:
+class Directory:
 
     def __init__(self, path):
-        self._rep = PythonPath(path)
+        self._rep = Path(path)
 
     def __str__(self):
-        return self.full_path
+        return self.path
 
     def __repr__(self):
         return self.__str__()
@@ -17,20 +17,8 @@ class Path:
         return self._rep.name
 
     @property
-    def stem(self):
-        return self._rep.stem
-
-    @property
-    def suffix(self):
-        return self._rep.suffix
-
-    @property
-    def full_path(self):
+    def path(self):
         return self._rep.parts[0] + '\\'.join(self._rep.parts[1:])
-
-    @property
-    def directory_path(self):
-        return self._rep.parts[0] + '\\'.join(self._rep.parts[1: len(self._rep.parts) - 1])
 
     def exists(self):
         return self._rep.exists()
@@ -39,10 +27,7 @@ class Path:
         return directory_name in self._rep.parts
 
     def join(self, path):
-        return Path(self.full_path + '\\' + path)
-
-    def rename(self, path):
-        return Path(self.full_path + '\\' + path)
+        return Path(self.path + '\\' + path)
 
     def has_file(self, name):
         return len([path for path in self.get_files() if path.name == name]) > 0
@@ -50,7 +35,7 @@ class Path:
     def get_file(self, name):
         paths = [path for path in self.get_files() if path.name == name]
         if len(paths) == 0:
-            raise ValueError('Could not find file ' + name + ' in directory ' + self.full_path)
+            raise ValueError('Could not find file ' + name + ' in directory ' + self.path)
         return paths[0]
 
     def get_files(self):
@@ -62,7 +47,7 @@ class Path:
     def get_directory(self, name):
         directories = [directory for directory in self.get_directories() if directory.name == name]
         if len(directories) == 0:
-            raise ValueError('Could not find file ' + name + ' in directory ' + self.full_path)
+            raise ValueError('Could not find file ' + name + ' in directory ' + self.path)
         return directories[0]
 
     def get_directories(self):
---
+++ io/path/file.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+
+
+class File:
+
+    def __init__(self, path):
+        self._rep = Path(path)
+
+    def __str__(self):
+        return self.path
+
+    def __repr__(self):
+        return self.__str__()
+
+    @property
+    def name(self):
+        return self._rep.name
+
+    @property
+    def stem(self):
+        return self._rep.stem
+
+    @property
+    def suffix(self):
+        return self._rep.suffix
+
+    @property
+    def path(self):
+        return self._rep.parts[0] + '\\'.join(self._rep.parts[1:])
+
+    @property
+    def directory_path(self):
+        return self._rep.parts[0] + '\\'.join(self._rep.parts[1: len(self._rep.parts) - 1])
+
+    def exists(self):
+        return self._rep.exists()
