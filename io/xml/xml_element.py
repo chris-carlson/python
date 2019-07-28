ea30ef815bc08e712a0cc02f0a6267864d254e53
Chris Carlson <Chris Carlson>
2019-07-28 14:49:45 -0400

    Added more functions for XmlElement

--- io/xml/xml_element.py
+++ io/xml/xml_element.py
@@ -3,6 +3,8 @@ import xml.etree.ElementTree as ET
 from typing import Dict, List
 from xml.etree.ElementTree import Element
 
+from cac.finder import Finder
+
 class XmlElement:
 
     def __init__(self, tag: str, attributes: Dict[str, str] = {}) -> None:
@@ -37,18 +39,44 @@ class XmlElement:
     def text(self, text) -> None:
         self._text = text
 
-    def find_by_tag(self, tag: str) -> List['XmlElement']:
+    def get_one_by_tag(self, tag: str) -> 'XmlElement':
+        matching_children: List[XmlElement] = self.get_all_by_tag(tag)
+        return Finder.find_only(matching_children)
+
+    def get_one_by_text(self, text: str) -> 'XmlElement':
+        matching_children: List[XmlElement] = self.get_all_by_text(text)
+        return Finder.find_only(matching_children)
+
+    def get_all_by_tag(self, tag: str) -> List['XmlElement']:
         matching_children: List[XmlElement] = []
         for child in self._children:
             if child.tag == tag:
                 matching_children.append(child)
-            matching_children.extend(child.find_by_tag(tag))
         return matching_children
 
-    def find_by_text(self, text: str) -> List['XmlElement']:
+    def get_all_by_text(self, text: str) -> List['XmlElement']:
         matching_children: List[XmlElement] = []
         for child in self._children:
             if child.text == text:
                 matching_children.append(child)
-            matching_children.extend(child.find_by_text(text))
+        return matching_children
+
+    def find_one_by_tag(self, tag: str) -> 'XmlElement':
+        matching_children: List[XmlElement] = self.find_all_by_tag(tag)
+        return Finder.find_only(matching_children)
+
+    def find_one_by_text(self, text: str) -> 'XmlElement':
+        matching_children: List[XmlElement] = self.find_all_by_text(text)
+        return Finder.find_only(matching_children)
+
+    def find_all_by_tag(self, tag: str) -> List['XmlElement']:
+        matching_children: List[XmlElement] = self.get_all_by_tag(tag)
+        for child in self._children:
+            matching_children.extend(child.find_all_by_tag(tag))
+        return matching_children
+
+    def find_all_by_text(self, text: str) -> List['XmlElement']:
+        matching_children: List[XmlElement] = self.get_all_by_text(text)
+        for child in self._children:
+            matching_children.extend(child.find_all_by_text(text))
         return matching_children
