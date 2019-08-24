2189114f56d4d293df37655ff2f2e080eed8e9f1
Chris Carlson <Chris Carlson>
2019-08-24 16:47:30 -0400

    Adding functions to get columns in worksheets

--- excel/worksheet.py
+++ excel/worksheet.py
@@ -19,3 +19,13 @@ class Worksheet:
     @property
     def title(self) -> str:
         return self._title
+
+    def get_column_by_index(self, index: int) -> List[object]:
+        return [row[index] for row in self._rows]
+
+    def get_column_by_letters(self, letters: str) -> List[object]:
+        index: int = 0
+        for index in range(0, len(letters)):
+            letter: str = letters[index].upper()
+            index += (ord(letter) - 65) + (index * 26)
+        return [row[index] for row in self._rows]
