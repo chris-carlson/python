c06d59fbe8f7f6731f56c490a913a94e0d28c185
Chris Carlson <Chris Carlson>
2019-12-01 19:39:18 -0500

    Added missing excel classes

---
+++ io/excel/row.py
@@ -0,0 +1,30 @@
+from typing import List, TypeVar
+
+from cac.io.excel.letter_converter import LetterConverter
+
+E = TypeVar('E')
+
+class Row(List[E]):
+
+    def __init__(self, rep: List[object]) -> None:
+        self._rep: List[object] = rep
+
+    def __str__(self) -> str:
+        return str(self._rep)
+
+    def __repr__(self) -> str:
+        return self.__str__()
+
+    def __len__(self) -> int:
+        return len(self._rep)
+
+    def __iter__(self) -> object:
+        for cell in self._rep:
+            yield cell
+
+    def __getitem__(self, index: int) -> object:
+        return self._rep[index]
+
+    def get_cell(self, letter: str) -> object:
+        index: int = LetterConverter.convert_letter(letter)
+        return self._rep[index]
--- timer.py
+++ timer.py
@@ -4,15 +4,15 @@ class Timer:
 
     def __init__(self) -> None:
         self._start: float = 0.0
-        self._end: float = 0.0
+        self._stop: float = 0.0
 
     def start(self) -> None:
         self._start = time.time()
 
-    def end(self) -> None:
-        self._end = time.time()
+    def stop(self) -> None:
+        self._stop = time.time()
 
     def get_time(self) -> float:
-        if self._start == 0 or self._end == 0 or self._start > self._end:
+        if self._start == 0 or self._stop == 0 or self._start > self._stop:
             raise ValueError('Timer has not been started and stopped')
-        return self._end - self._start
+        return self._stop - self._start
