29478a499488f92fa78d2cff8fd05dfc1501853e
Chris Carlson <ccarlson9311@gmail.com>
2020-03-27 20:34:59 -0400

    Added pair list

---
+++ collections/pair.py
@@ -0,0 +1,14 @@
+from typing import Tuple, TypeVar
+
+K = TypeVar('K')
+V = TypeVar('V')
+
+class Pair(Tuple[K, V]):
+
+    @property
+    def key(self) -> K:
+        return self[0]
+
+    @property
+    def value(self) -> V:
+        return self[1]
---
+++ collections/pair_list.py
@@ -0,0 +1,15 @@
+from typing import List, TypeVar
+
+from cac.collections.pair import Pair
+
+K = TypeVar('K')
+V = TypeVar('V')
+
+class PairList(List[Pair[K, V]]):
+
+    def __init__(self, elements: List[Pair[K, V]]) -> None:
+        for element in elements:
+            self.append(element)
+
+    def add(self, key: K, value: V) -> None:
+        self.append((key, value))
