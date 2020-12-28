620a4e0984771ef69a27ca67017b2fbec8d2feef
Chris Carlson <Chris Carlson>
2020-12-28 09:31:08 -0500

    Add reverse option for sorter

--- sorter.py
+++ sorter.py
@@ -5,11 +5,12 @@ class Sorter:
 
     # noinspection PyUnresolvedReferences,PyTypeChecker
     @staticmethod
-    def sort(collection: Sequence[object], lambda_function: Callable[[object], object] = lambda item: item) -> List[
-        object]:
+    def sort(collection: Sequence[object], lambda_function: Callable[[object], object] = lambda item: item, reverse: bool = False) -> List[object]:
         if issubclass(type(collection), dict):
             sorted_list: List[object] = list(collection.items())
         else:
             sorted_list: List[object] = list(collection)
         sorted_list.sort(key=lambda_function)
+        if reverse:
+            sorted_list.reverse()
         return sorted_list
