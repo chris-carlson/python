21f726cb2d148c94ae7648e4c76f5040a9fbbe80
Chris Carlson <Chris Carlson>
2020-12-31 13:50:14 -0500

    Check for duplicate argument and flag definitions

--- finder.py
+++ finder.py
@@ -1,4 +1,4 @@
-from typing import List, TypeVar
+from typing import List, Set, TypeVar
 
 E = TypeVar('E')
 
@@ -23,3 +23,15 @@ class Finder:
         if len(filtered_list) > 1:
             raise ValueError('Found multiple matches in the list')
         return filtered_list[0]
+
+    @staticmethod
+    def find_duplicates(item_list: List[E]) -> Set[E]:
+        search_list: List[E] = item_list[:]
+        encountered_items: Set[E] = set()
+        duplicated_items: Set[E] = set()
+        while len(search_list) > 0:
+            item: E = search_list.pop(0)
+            if item in encountered_items:
+                duplicated_items.add(item)
+            encountered_items.add(item)
+        return duplicated_items
--- help/command.py
+++ help/command.py
@@ -1,6 +1,7 @@
 from typing import List, Set
 
 from cac.color import Color
+from cac.finder import Finder
 from cac.help.argument import Argument
 from cac.help.flag import Flag
 
@@ -11,6 +12,7 @@ class Command:
         self._name: str = name
         self._flags: List[Flag] = flags
         self._arguments: List[Argument] = arguments
+        self._validate_input()
 
     @property
     def flags(self) -> List[Flag]:
@@ -19,6 +21,16 @@ class Command:
     @property
     def arguments(self) -> List[Argument]:
         return self._arguments
+
+    def _validate_input(self) -> None:
+        flag_names: List[str] = [flag.names[0] for flag in self._flags] + [flag.names[1] for flag in self._flags]
+        duplicate_flag_names: Set[str] = Finder.find_duplicates(flag_names)
+        if len(duplicate_flag_names) > 0:
+            raise ValueError('Duplicate flag definitions found: ' + str(duplicate_flag_names))
+        argument_names: List[str] = [argument.name for argument in self._arguments]
+        duplicate_argument_names: Set[str] = Finder.find_duplicates(argument_names)
+        if len(duplicate_argument_names) > 0:
+            raise ValueError('Duplicate argument definitions found: ' + str(duplicate_argument_names))
 
     def print_help(self) -> None:
         command: str = Color.highlight_text(self._name, Color.FORE['Magenta'])
