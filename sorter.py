251815c1b773ae432f58ce0aa56587ab8628e28d
Chris Carlson <ccarlson9311@gmail.com>
2021-10-23 10:10:19 -0400

    Split sorter into specific list and dict classes

--- sorter.py
+++ sorter.py
@@ -1,16 +1,28 @@
-from typing import Callable, List, Sequence
+from typing import Any, Callable, Dict, List, Tuple, TypeVar
+
+E = TypeVar('E')
+K = TypeVar('K')
+V = TypeVar('V')
 
 
-class Sorter:
+class ListSorter:
 
-    # noinspection PyUnresolvedReferences,PyTypeChecker
     @staticmethod
-    def sort(collection: Sequence[object], lambda_function: Callable[[object], object] = lambda item: item,
-            reverse: bool = False) -> List[object]:
-        if issubclass(type(collection), dict):
-            sorted_list: List[object] = list(collection.items())
-        else:
-            sorted_list: List[object] = list(collection)
+    def sort(collection: List[E], lambda_function: Callable[[E], Any] = lambda item: item, reverse: bool = False) -> \
+            List[E]:
+        sorted_list: List[E] = list(collection)
+        sorted_list.sort(key=lambda_function)
+        if reverse:
+            sorted_list.reverse()
+        return sorted_list
+
+
+class DictSorter:
+
+    @staticmethod
+    def sort(collection: Dict[K, V], lambda_function: Callable[[Tuple[K, V]], Any] = lambda item: item,
+            reverse: bool = False) -> List[Tuple[K, V]]:
+        sorted_list: List[Tuple[K, V]] = list(collection.items())
         sorted_list.sort(key=lambda_function)
         if reverse:
             sorted_list.reverse()
