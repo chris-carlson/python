562524868b1ea851ecf08346bc96b6aac0972362
chris-carlson <ccarlson9311@gmail.com>
2023-09-10 17:35:41 -0400

    Create install script for pip packages

---
+++ install.sh
@@ -0,0 +1,10 @@
+pip install beautifulsoup4
+pip install colorama
+pip install lxml
+pip install openpyxl
+pip install pyperclip
+pip install python-dateutil
+pip install requests
+pip install selenium
+pip install Unidecode
+pip install ytmusicapi
